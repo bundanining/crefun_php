9524d92de1e6576f9341ffa93567fa2125121cf2
오정민 <dhkdn4042@naver.com>
2017-07-12 14:55:48 +0900

    Upload 컨트롤러 제

--- application/controllers/Board_c.php
+++ application/controllers/Board_c.php
@@ -1,6 +1,5 @@
 <?php
   defined('BASEPATH') OR exit('No direct script access allowed');
-require_once 'Upload.php';
 class Board_c extends CI_Controller {
 	function __construct() {
 			parent::__construct();
