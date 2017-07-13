6f3eb8b081c79c6e7d888d7c49f8300fefb30fab
오정민 <dhkdn4042@naver.com>
2017-07-13 13:34:44 +0900

    no message

--- application/views/b_list.php
+++ application/views/b_list.php
@@ -58,7 +58,7 @@
             </tr>
           </tfoot>
         </table>
-        <form class="form-group form-inline" action="/">
+        <form class="form-group form-inline" method="get" action="/">
             <select class="form-control" id="searchItem">
               <option value="b_title">제목</option>
               <option value="b_content">내용</option>
