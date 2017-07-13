d152a6950a4bac39967d3667baa3769013b2e8e0
오정민 <dhkdn4042@naver.com>
2017-07-13 14:15:59 +0900

    no message

--- application/config/routes.php
+++ application/config/routes.php
@@ -3,6 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 $route['default_controller'] = 'user';
 $route['board'] = 'board_c';
+$route['board/:num'] = 'board_c';
 $route['board/write'] = 'board_c/write';
 $route['board/delete/:num'] = 'board_c/delete/$1';
 $route['board/list/:num'] = 'board_c/detail/$1';
