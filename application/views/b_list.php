89ab4af0dc723dfcae8764fdf5cd053b2cce2434
오정민 <dhkdn4042@naver.com>
2017-07-13 14:04:28 +0900

    no message

--- application/views/b_list.php
+++ application/views/b_list.php
@@ -60,9 +60,9 @@
         </table>
         <form class="form-group form-inline" method="post" action="/index.php/board/search">
             <select class="form-control" name="searchItem" id="searchItem">
-              <option value="b_title">제목</option>
-              <option value="b_content">내용</option>
-              <option value="b_writer">작성자</option>
+              <option value="title">제목</option>
+              <option value="content">내용</option>
+              <option value="writer">작성자</option>
             </select>
             <input name="searchBox" id="searchBox" type="text" class="form-control" placeholder="검색">
             <button class="btn btn-default" type="submit">검색</button>
