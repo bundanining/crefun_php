e26d2b3563847e175b7a9af85c1bc93eba609441
오정민 <dhkdn4042@naver.com>
2017-07-13 14:03:30 +0900

    라우트 수

--- application/config/routes.php
+++ application/config/routes.php
@@ -3,11 +3,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 $route['default_controller'] = 'user';
 $route['board'] = 'board_c';
-$route['board/:num'] = 'board_c';
 $route['board/write'] = 'board_c/write';
 $route['board/delete/:num'] = 'board_c/delete/$1';
 $route['board/list/:num'] = 'board_c/detail/$1';
 $route['board/insert'] = 'board_c/insert';
+$route['board/search'] = 'board_c/search';
 $route['board/update/:num'] = 'board_c/update_view';
 $route['board/update_post/:num'] = 'board_c/update_post';
 $route['404_override'] = '';
