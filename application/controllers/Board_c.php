19d89f59a59a84cab4314b6f5c81a846e128d74b
오정민 <dhkdn4042@naver.com>
2017-07-13 16:12:23 +0900

    no message

--- application/controllers/Board_c.php
+++ application/controllers/Board_c.php
@@ -138,7 +138,7 @@ class Board_c extends CI_Controller {
 
     public function search() {
       //페이지네이션 게시글 5개씩 출력
-      $start = $this -> uri -> segment(4, 0);
+      $start = $this -> uri -> segment(3, 0);
       $config['per_page'] = 5;  // 한쪽에 표현될 아이템의 갯수
       print_r($start);
       //페이지네이션 게시글 5개씩 출력
@@ -158,8 +158,9 @@ class Board_c extends CI_Controller {
       $this->load->library('pagination');
       //페이지네이션 config
       $config['num_links'] = 2; // 쪽선택 몇개씩 보여줄것인지 2이면 1,2,3,4,5까지 보임
+      $config[‘reuse_query_string’] = FALSE;
       $config['use_page_numbers'] = TRUE; //URI 새그먼트는 페이징하는 아이템들의 시작 인덱스를 사용함. 실제 페이지 번호를 보여주고 싶다면, TRUE
-      $config['base_url'] = '/index.php/board/search?o='.$data['condition'].'&q='.$data['data']; //페이지네이션이 보여질 url
+      $config['base_url'] = '/index.php/board/search'; //페이지네이션이 보여질 url
       $config['total_rows'] = $count; //전체 행의 개수
       $this->pagination->initialize($config);
       $res = array(
