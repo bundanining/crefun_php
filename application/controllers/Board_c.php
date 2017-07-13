3add67df339cdaf1b68fa34696c3b40361b0d42d
오정민 <dhkdn4042@naver.com>
2017-07-13 15:38:44 +0900

    no message

--- application/controllers/Board_c.php
+++ application/controllers/Board_c.php
@@ -139,6 +139,7 @@ class Board_c extends CI_Controller {
     public function search() {
       //페이지네이션 게시글 5개씩 출력
       $start = $this -> uri -> segment(4, 0);
+      print_r($start);
       $config['per_page'] = 5;  // 한쪽에 표현될 아이템의 갯수
       //페이지네이션 게시글 5개씩 출력
       $limit = $config['per_page'];
