fd2f5a863b503c836acd70d9aec63fb1eabf3927
ggomi <dhkdn4042@naver.com>
2017-07-14 14:45:33 +0900

    스마트에디터 수정

--- application/views/b_write.php
+++ application/views/b_write.php
@@ -87,6 +87,7 @@ $(function(){
             // 모드 탭(Editor | HTML | TEXT) 사용 여부 (true:사용/ false:사용하지 않음)
             bUseModeChanger : true,
         }
+	});
 	
 	$('#confirm').click(function(){
        //id가 smarteditor인 textarea에 에디터에서 대입
