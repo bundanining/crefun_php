b5e48548423f5173135fb378861965d14253be67
ggomi <dhkdn4042@naver.com>
2017-07-14 15:05:28 +0900

    글 수정페이지에 스마트에디터 추가

--- application/views/b_update.php
+++ application/views/b_update.php
@@ -4,7 +4,7 @@
     <div class="panel panel-default">
       <div class="panel-heading" style="text-align: center;">글쓰기 페이지</div>
       <div class="panel-body">
-        <form action="/index.php/board/update_post/<?php echo $id ?>" method="post" onsubmit="return check()">
+        <form id="form" action="/index.php/board/update_post/<?php echo $id ?>" method="post" onsubmit="return check()">
           <table class="table">
             <thead>
               <tr>
@@ -13,9 +13,9 @@
             </thead>
             <tbody>
                 <tr>
-                  <th>
-                    <input type="text" id="content" name="content" value="<?php echo $content?>" style="width:400px; height:450px;">
-                  </th>
+                  <td>
+					<textarea rows="10" cols="20" id="content" name="content" value="<?php echo $content?>" style="width:200px; height:auto; display:none;"></textarea>
+				  </td>
                 </tr>
             </tbody>
           </table>
@@ -31,12 +31,32 @@
   </div>
 </body>
 <script type="text/javascript">
-function check() {
-    if($('#title').val() == "" || $('#content').val()== "" || $('#pw').val() == ""){
-      alert('모든 내용을 채워주세요!!');
-      return false;
-    }else {
-      return true;
-    }
-}
+$(function(){
+    //전역변수선언
+    var editor_object = [];
+     
+    nhn.husky.EZCreator.createInIFrame({
+        oAppRef: editor_object,
+        elPlaceHolder: "content",
+        sSkinURI: "/smartediter/SmartEditor2Skin.html",
+        htParams : {
+            // 툴바 사용 여부 (true:사용/ false:사용하지 않음)
+            bUseToolbar : true,            
+            // 입력창 크기 조절바 사용 여부 (true:사용/ false:사용하지 않음)
+            bUseVerticalResizer : true,    
+            // 모드 탭(Editor | HTML | TEXT) 사용 여부 (true:사용/ false:사용하지 않음)
+            bUseModeChanger : true,
+        }
+	});
+	
+	$('#confirm').click(function(){
+       //id가 smarteditor인 textarea에 에디터에서 대입
+       editor_object.getById["content"].exec("UPDATE_CONTENTS_FIELD", []);
+         
+       // 이부분에 에디터 validation 검증
+
+       //폼 submit
+       $("#form").submit();
+    });
+});
 </script>
