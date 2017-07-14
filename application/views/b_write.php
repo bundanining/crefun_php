b4af2a63205ac33b54be3c1e313f2af7644bcbad
ggomi <dhkdn4042@naver.com>
2017-07-14 11:42:55 +0900

    스마트에디터 추가

--- application/views/b_write.php
+++ application/views/b_write.php
@@ -13,9 +13,9 @@
             </thead>
             <tbody>
                 <tr>
-                  <th>
-                    <input type="text" id="content" name="content" value="" style="width:400px; height:450px;">
-                  </th>
+                  <td>
+				    <textarea rows="10" cols="30" id="content" name="content" style="width:650px; height:350px; "></textarea>
+                  </td>
                 </tr>
             </tbody>
           </table>
@@ -40,6 +40,7 @@ function check() {
       return true;
     }
 }
+/*
 $(document).read(function(){
   $('#form').submit(function(e){
     e.preventDefault();
@@ -67,5 +68,36 @@ $(document).read(function(){
     });
     return false;
   });
+});*/
+$(function(){
+      nhn.husky.EZCreator.createInIFrame({
+          oAppRef: oEditors,
+          elPlaceHolder: "content", //textarea에서 지정한 id와 일치해야 합니다. 
+          //SmartEditor2Skin.html 파일이 존재하는 경로
+          sSkinURI: "./smartediter/SmartEditor2Skin.html",  
+          htParams : {
+              // 툴바 사용 여부 (true:사용/ false:사용하지 않음)
+              bUseToolbar : true,             
+              // 입력창 크기 조절바 사용 여부 (true:사용/ false:사용하지 않음)
+              bUseVerticalResizer : true,     
+              // 모드 탭(Editor | HTML | TEXT) 사용 여부 (true:사용/ false:사용하지 않음)
+              bUseModeChanger : true,         
+              fOnBeforeUnload : function(){
+                   
+              }
+          }, 
+          fOnAppLoad : function(){
+              //기존 저장된 내용의 text 내용을 에디터상에 뿌려주고자 할때 사용
+              oEditors.getById["content"].exec("PASTE_HTML", ["기존 DB에 저장된 내용을 에디터에 적용할 문구"]);
+          },
+          fCreator: "createSEditor2"
+      });
+      
+      //저장버튼 클릭시 form 전송
+      $("#save").click(function(){
+          oEditors.getById["confirm"].exec("UPDATE_CONTENTS_FIELD", []);
+          $("#form").submit();
+      });    
 });
+
 </script>
